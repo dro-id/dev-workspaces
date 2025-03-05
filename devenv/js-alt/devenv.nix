bc067fe38b14291ec7dd99ae1bcde2909e708bd8
Marc Guillen <marc@0x01.ooo>
2025-03-05 17:21:24 +0100

    Adding a new alternative javascript workspace based on deno runtime and tooling

---
+++ devenv/js-alt/devenv.nix
@@ -0,0 +1,140 @@
+{ pkgs, lib, ... }: 
+
+let
+  enableAllPrecommitHooks = false;
+in
+{
+
+  name = "Deno Development Sandbox";
+
+  # Enable devcontainer support
+  # Note: features are not used and replaced by devenv
+  devcontainer = {
+    enable = true;
+    settings = {
+      customizations = {
+        vscode = {
+          settings = {
+            # Standard
+            update.showReleaseNotes = false;
+            window.commandCenter = false;
+            workbench.colorTheme = "GitHub Dark Default";
+            workbench.preferredDarkColorTheme = "GitHub Dark Default";
+            workbench.preferredLightColorTheme = "GitHub Light Default";
+            workbench.preferredHighContrastColorTheme = "GitHub Dark High Contrast";
+            workbench.preferredHighContrastLightColorTheme = "GitHub Light High Contrast";
+            editor.formatOnSave = true;
+            editor.formatOnPaste = true;
+            editor.minimap.autohide = true;
+            asciidoc.antora.enableAntoraSupport = false;
+            asciidoc.extensions.enableKroki = true;
+          };
+          extensions = [
+            # Standard
+            "github.github-vscode-theme"
+            "github.codespaces"
+            "github.copilot"
+            "github.copilot-chat"
+            "asciidoctor.asciidoctor-vscode"
+            "mkhl.direnv"
+            # Stack specific
+            "denoland.vscode-deno"
+            "bradlc.vscode-tailwindcss"
+          ]; 
+        };
+        codespaces = {
+          "openFiles" = [
+            # Open README at launch
+            "README.md"
+          ];
+        };
+      };
+      # Capacity requested for devcontainer
+      hostRequirements = {
+        cpus = 2;
+        memory = "8gb";
+        storage = "32gb";
+      };
+      # Optional: Secret asked when starting the devcontainer
+      secrets = {};
+    };
+  };
+  
+  # Enable starship for a better prompt
+  # Will load a project starship.toml
+  starship = {
+    enable = true;
+    config.enable = true;
+  };
+
+  # Common packages for developers
+  difftastic.enable = true;
+  packages = with pkgs; [ 
+    # Standard
+    curl
+    git
+    gh
+    jq
+    deno
+  ];
+
+  # Pre-commit hooks: Deno
+  pre-commit.hooks = {
+    deno-fmt.enable = enableAllPrecommitHooks;
+    deno-lint.enable = enableAllPrecommitHooks;
+  };
+
+  # Additional services (attached resources)
+  # No additional services, this is a sandbox
+  services = {};
+
+  # Environment variables
+  dotenv.enable = false;
+  env = {
+    DEVENV_STACK = "javascript";
+  };
+ 
+  # Special hosts
+  # Can be used to mock external attached resources
+  hosts = {
+    "locahost" = "127.0.0.1";
+  };
+
+  # Scripts: Javascript (Deno) 
+  # Can be used as aliases
+  scripts = {
+    # Workflow shortcuts
+    new.exec = ''
+      printf "\033[1m%s\033[0m\n" "Create New Project"
+      printf " ! Organisation: "
+      read -r my_orga
+      printf " ! Repository Name: "
+      read -r my_repo_name
+      printf " ✓ Creating a new repo \033[3m%s\033[0m in organisation \033[3m%s\033[0m based on template \033[3m%s\033[0m!\n" "''${my_repo_name}" "''${my_orga}" "''${DEVENV_STACK}-template" 
+      gh create "''${my_repo_name}" --clone --template "https://github.com/''${my_orga}/''${DEVENV_STACK}-workspace.git"
+    '';
+    init.exec = ''
+      deno init
+    '';
+    run.exec = ''
+      deno run $*
+    '';
+    test.exec = ''
+      deno test
+    '';
+    fmt.exec = ''
+      deno fmt
+    '';
+    lint.exec = ''
+      deno lint
+    '';
+  };
+
+  # Startup commands
+  enterShell = ''
+    clear
+    echo "【ツ】Welcome to your 🦕 Deno Sandbox!"
+    echo "deno version: $(deno --version)"
+  '';
+
+}
--- flake.nix
+++ flake.nix
@@ -51,11 +51,12 @@
         config.allowUnfree = true;
       };
       
-        devenv.shells.nix               = { imports = [ ./devenv/nix/devenv.nix ]; };
-        devenv.shells.rust-stable       = { imports = [ ./devenv/rs/devenv.nix ]; };
-        devenv.shells.python-stable     = { imports = [ ./devenv/py/devenv.nix ]; };
-        devenv.shells.javascript-stable = { imports = [ ./devenv/js/devenv.nix ]; };
-        devenv.shells.terraform-stable  = { imports = [ ./devenv/tf/devenv.nix ]; };
+        devenv.shells.nix                   = { imports = [ ./devenv/nix/devenv.nix ]; };
+        devenv.shells.rust-stable           = { imports = [ ./devenv/rs/devenv.nix ]; };
+        devenv.shells.python-stable         = { imports = [ ./devenv/py/devenv.nix ]; };
+        devenv.shells.javascript-stable     = { imports = [ ./devenv/js/devenv.nix ]; };
+        devenv.shells.javascript-alt-stable = { imports = [ ./devenv/js-alt/devenv.nix ]; }; # Alternative based on deno
+        devenv.shells.terraform-stable      = { imports = [ ./devenv/tf/devenv.nix ]; };
         
         devShells.default = config.devShells.nix;
         
